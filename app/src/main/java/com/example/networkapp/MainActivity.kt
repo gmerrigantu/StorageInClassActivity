fd1c1984369d263f8f5d45e2ae0edc5f747a1cfa
grantmerrigan <gmerrigan13@gmail.com>
2025-04-08 12:00:37 -0400

    Added save comic functionality

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.networkapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -14,6 +15,10 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import kotlin.concurrent.write
 
 // TODO (1: Fix any bugs)
 // TODO (2: Add function saveComic(...) to save comic info when downloaded
@@ -27,6 +32,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var numberEditText: EditText
     lateinit var showButton: Button
     lateinit var comicImageView: ImageView
+    private val filename = "comic.json";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +50,21 @@ class MainActivity : AppCompatActivity() {
             downloadComic(numberEditText.text.toString())
         }
 
+
+        val file = File(filesDir, filename)
+
+        if (file.exists()) {
+            try {
+                val fileInputStream = file.inputStream()
+                val jsonString = fileInputStream.bufferedReader().use { it.readText() }
+                val comicObject = JSONObject(jsonString)
+                showComic(comicObject)
+                Log.d("MainActivity", "Comic loaded from: ${file.absolutePath}")
+            } catch (e: IOException) {
+                Log.e("MainActivity", "Error loading comic: ${e.message}")
+            }
+        }
+
     }
 
     // Fetches comic from web as JSONObject
@@ -52,7 +73,7 @@ class MainActivity : AppCompatActivity() {
         requestQueue.add (
             JsonObjectRequest(url
                 , {showComic(it)}
-                , {}
+                , { Log.e("MainActivity", "Error downloading comic: ${it.message}")}
             )
         )
     }
@@ -61,12 +82,25 @@ class MainActivity : AppCompatActivity() {
     private fun showComic (comicObject: JSONObject) {
         titleTextView.text = comicObject.getString("title")
         descriptionTextView.text = comicObject.getString("alt")
+        val imageUrl = comicObject.getString("img")
+        Log.d("MainActivity", "Image URL: $imageUrl")
         Picasso.get().load(comicObject.getString("img")).into(comicImageView)
+        saveComic(comicObject)
     }
 
     // Implement this function
     private fun saveComic(comicObject: JSONObject) {
 
+        val file = File(filesDir, filename)
+
+        try {
+            val fileOutputStream = FileOutputStream(file)
+            fileOutputStream.write(comicObject.toString().toByteArray())
+            fileOutputStream.close()
+            Log.d("MainActivity", "Comic saved to: ${file.absolutePath}")
+        } catch (e: IOException) {
+            Log.e("MainActivity", "Error saving comic: ${e.message}")
+        }
     }
 
 
